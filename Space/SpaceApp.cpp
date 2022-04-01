4f9eeb473067d7ff0d06ce48555909e891156b77
Deye Lei <deye.lei76@myhunter.cuny.edu>
2022-04-01 16:48:58 -0400

    glad added

--- Space/SpaceApp.cpp
+++ Space/SpaceApp.cpp
@@ -63,6 +63,17 @@ namespace Space
 		
 		)";
 
+		std::string fragmentShader = R"(
+			#version 330 core 
+			out vec4 fragColor
+
+			void main()
+			{
+				fragColor = vec4(0.0f, 1.0f, 0.0f, 1.0f);
+			}
+		
+		)";
+
 		while (true)
 		{
 			OnUpdate();
